ea33d0c54962dda262546d7cf19069e182cfd5a9
Jazmin Solorzano <jazminsol@yahoo.com>
2021-02-09 18:57:45 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,3 +1,13 @@
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
 current_time = Time.now
-current_time.to_s
-current_time.to_i
+current_time = current_time.to_i
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
