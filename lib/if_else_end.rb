bdba9d18f7bc3cfa72c52eb466f506349def8c15
Jazmin Solorzano <jazminsol@yahoo.com>
2021-02-09 18:31:14 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -11,3 +11,11 @@ if run_code_inside
   puts "code inside"
 end
 puts "code after if...end"
+
+chance_of_rain = 0.2
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
