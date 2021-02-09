5189a5f6209debc0448292c6a65f343e1ad2defe
Jazmin Solorzano <jazminsol@yahoo.com>
2021-02-09 18:24:53 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,13 @@
+if true
+  puts"This code runs!"
+end
+if false
+  puts "This code does not run. You will not see this in IRB."
+end
+
 run_code_inside = true
 puts "Code before if...end"
 if run_code_inside
   puts "code inside"
 end
-puts "Code after if...end"
+puts "code after if...end"
